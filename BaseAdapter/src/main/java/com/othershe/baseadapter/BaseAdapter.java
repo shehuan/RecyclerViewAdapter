625ab1d22c99378c7aefba78fb97a2b5da6fe99d
Othershe <shehuan320@163.com>
2016-09-09 18:40:39 +0800

    add multi item support

--- BaseAdapter/src/main/java/com/othershe/baseadapter/BaseAdapter.java
+++ BaseAdapter/src/main/java/com/othershe/baseadapter/BaseAdapter.java
@@ -1,76 +1,40 @@
 package com.othershe.baseadapter;
 
 import android.content.Context;
-import android.support.v7.widget.GridLayoutManager;
-import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.RelativeLayout;
 
-import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Author: Othershe
- * Time: 2016/8/29 09:46
+ * Time: 2016/9/9 15:52
  */
-public abstract class BaseAdapter<T> extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
-    public static final int TYPE_COMMON_VIEW = 100001;//普通类型 Item
-    public static final int TYPE_FOOTER_VIEW = 100002;//footer类型 Item
-    public static final int TYPE_EMPTY_VIEW = 100003;//empty view，即初始化加载时的提示View
+public abstract class BaseAdapter<T> extends LBaseAdapter<T> {
+    protected OnItemClickListeners<T> mItemClickListener;
 
-    private OnLoadMoreListener mLoadMoreListener;
-    private OnItemClickListeners<T> mItemClickListener;
-
-    protected Context mContext;
-    protected List<T> mDatas;
-    private boolean mOpenLoadMore;//是否开启加载更多
-    private boolean isAutoLoadMore = true;//是否自动加载，当数据不满一屏幕会自动加载
-
-    private View mLoadingView;
-    private View mLoadFailedView;
-    private View mLoadEndView;
-    private View mEmptyView;
-    private RelativeLayout mFooterLayout;//footer view
+    public BaseAdapter(Context context, List<T> datas, boolean isOpenLoadMore) {
+        super(context, datas, isOpenLoadMore);
+    }
 
     protected abstract void convert(ViewHolder holder, T data);
 
     protected abstract int getItemLayoutId();
 
-    public BaseAdapter(Context context, List<T> datas, boolean isOpenLoadMore) {
-        mContext = context;
-        mDatas = datas == null ? new ArrayList<T>() : datas;
-        mOpenLoadMore = isOpenLoadMore;
-    }
-
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        ViewHolder viewHolder = null;
-        switch (viewType) {
-            case TYPE_COMMON_VIEW:
-                viewHolder = ViewHolder.create(mContext, getItemLayoutId(), parent);
-                break;
-            case TYPE_FOOTER_VIEW:
-                if (mFooterLayout == null) {
-                    mFooterLayout = new RelativeLayout(mContext);
-                }
-                viewHolder = ViewHolder.create(mFooterLayout);
-                break;
-            case TYPE_EMPTY_VIEW:
-                viewHolder = ViewHolder.create(mEmptyView);
-                break;
+        if (isCommonItemView(viewType)) {
+            return ViewHolder.create(mContext, getItemLayoutId(), parent);
         }
-        return viewHolder;
+        return super.onCreateViewHolder(parent, viewType);
     }
 
     @Override
     public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
-        switch (holder.getItemViewType()) {
-            case TYPE_COMMON_VIEW:
-                bindCommonItem(holder, position);
-                break;
+        int viewType = holder.getItemViewType();
+        if (isCommonItemView(viewType)) {
+            bindCommonItem(holder, position);
         }
     }
 
@@ -87,276 +51,8 @@ public abstract class BaseAdapter<T> extends RecyclerView.Adapter<RecyclerView.V
     }
 
     @Override
-    public int getItemCount() {
-        if (mDatas.isEmpty() && mEmptyView != null) {
-            return 1;
-        }
-        return mDatas.size() + getFooterViewCount();
-    }
-
-    @Override
-    public int getItemViewType(int position) {
-        if (mDatas.isEmpty() && mEmptyView != null) {
-            return TYPE_EMPTY_VIEW;
-        }
-
-        if (isFooterView(position)) {
-            return TYPE_FOOTER_VIEW;
-        }
-
+    protected int getViewType(int position, T data) {
         return TYPE_COMMON_VIEW;
-    }
-
-    /**
-     * 根据positiond得到data
-     *
-     * @param position
-     * @return
-     */
-    public T getItem(int position) {
-        if (mDatas.isEmpty()) {
-            return null;
-        }
-        return mDatas.get(position);
-    }
-
-    /**
-     * 是否是FooterView
-     *
-     * @param position
-     * @return
-     */
-    private boolean isFooterView(int position) {
-        return mOpenLoadMore && position >= getItemCount() - 1;
-    }
-
-    /**
-     * StaggeredGridLayoutManager模式时，FooterView可占据一行
-     *
-     * @param holder
-     */
-    @Override
-    public void onViewAttachedToWindow(RecyclerView.ViewHolder holder) {
-        super.onViewAttachedToWindow(holder);
-        if (isFooterView(holder.getLayoutPosition())) {
-            ViewGroup.LayoutParams lp = holder.itemView.getLayoutParams();
-
-            if (lp != null && lp instanceof StaggeredGridLayoutManager.LayoutParams) {
-                StaggeredGridLayoutManager.LayoutParams p = (StaggeredGridLayoutManager.LayoutParams) lp;
-                p.setFullSpan(true);
-            }
-        }
-    }
-
-    /**
-     * GridLayoutManager模式时， FooterView可占据一行，判断RecyclerView是否到达底部
-     *
-     * @param recyclerView
-     */
-    @Override
-    public void onAttachedToRecyclerView(RecyclerView recyclerView) {
-        super.onAttachedToRecyclerView(recyclerView);
-        final RecyclerView.LayoutManager layoutManager = recyclerView.getLayoutManager();
-        if (layoutManager instanceof GridLayoutManager) {
-            final GridLayoutManager gridManager = ((GridLayoutManager) layoutManager);
-            gridManager.setSpanSizeLookup(new GridLayoutManager.SpanSizeLookup() {
-                @Override
-                public int getSpanSize(int position) {
-                    if (isFooterView(position)) {
-                        return gridManager.getSpanCount();
-                    }
-                    return 1;
-                }
-            });
-        }
-        startLoadMore(recyclerView, layoutManager);
-    }
-
-
-    /**
-     * 判断列表是否滑动到底部
-     *
-     * @param recyclerView
-     * @param layoutManager
-     */
-    private void startLoadMore(RecyclerView recyclerView, final RecyclerView.LayoutManager layoutManager) {
-        if (!mOpenLoadMore || mLoadMoreListener == null) {
-            return;
-        }
-
-        recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
-            @Override
-            public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
-                super.onScrollStateChanged(recyclerView, newState);
-                if (newState == RecyclerView.SCROLL_STATE_IDLE) {
-                    if (!isAutoLoadMore && findLastVisibleItemPosition(layoutManager) + 1 == getItemCount()) {
-                        scrollLoadMore();
-                    }
-                }
-            }
-
-            @Override
-            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
-                super.onScrolled(recyclerView, dx, dy);
-                if (isAutoLoadMore && findLastVisibleItemPosition(layoutManager) + 1 == getItemCount()) {
-                    scrollLoadMore();
-                } else if (isAutoLoadMore) {
-                    isAutoLoadMore = false;
-                }
-            }
-        });
-    }
-
-    /**
-     * 到达底部开始刷新
-     */
-    private void scrollLoadMore() {
-        if (mFooterLayout.getChildAt(0) == mLoadingView) {
-            mLoadMoreListener.onLoadMore(false);
-        }
-    }
-
-    private int findLastVisibleItemPosition(RecyclerView.LayoutManager layoutManager) {
-        if (layoutManager instanceof LinearLayoutManager) {
-            return ((LinearLayoutManager) layoutManager).findLastVisibleItemPosition();
-        } else if (layoutManager instanceof StaggeredGridLayoutManager) {
-            int[] lastVisibleItemPositions = ((StaggeredGridLayoutManager) layoutManager).findLastVisibleItemPositions(null);
-            return Util.findMax(lastVisibleItemPositions);
-        }
-        return -1;
-    }
-
-    /**
-     * 清空footer view
-     */
-    private void removeFooterView() {
-        mFooterLayout.removeAllViews();
-    }
-
-    /**
-     * 添加新的footer view
-     *
-     * @param footerView
-     */
-    private void addFooterView(View footerView) {
-        if (footerView == null) {
-            return;
-        }
-
-        if (mFooterLayout == null) {
-            mFooterLayout = new RelativeLayout(mContext);
-        }
-        removeFooterView();
-        RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
-                ViewGroup.LayoutParams.WRAP_CONTENT);
-        mFooterLayout.addView(footerView, params);
-    }
-
-    /**
-     * 刷新加载更多的数据
-     *
-     * @param datas
-     */
-    public void setLoadMoreData(List<T> datas) {
-        int size = mDatas.size();
-        mDatas.addAll(datas);
-        notifyItemInserted(size);
-    }
-
-    /**
-     * 下拉刷新，得到的新数据查到原数据起始
-     *
-     * @param datas
-     */
-    public void setData(List<T> datas) {
-        mDatas.addAll(0, datas);
-        notifyDataSetChanged();
-    }
-
-    /**
-     * 初次加载、或下拉刷新要替换全部旧数据时刷新数据
-     *
-     * @param datas
-     */
-    public void setNewData(List<T> datas) {
-        mDatas.clear();
-        mDatas.addAll(datas);
-        notifyDataSetChanged();
-    }
-
-    /**
-     * 初始化加载中布局
-     *
-     * @param loadingView
-     */
-    public void setLoadingView(View loadingView) {
-        mLoadingView = loadingView;
-        addFooterView(mLoadingView);
-    }
-
-    public void setLoadingView(int loadingId) {
-        setLoadingView(Util.inflate(mContext, loadingId));
-    }
-
-    /**
-     * 初始加载失败布局
-     *
-     * @param loadFailedView
-     */
-    public void setLoadFailedView(View loadFailedView) {
-        if (loadFailedView == null) {
-            return;
-        }
-        mLoadFailedView = loadFailedView;
-        addFooterView(mLoadFailedView);
-        mLoadFailedView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                addFooterView(mLoadingView);
-                mLoadMoreListener.onLoadMore(true);
-            }
-        });
-    }
-
-    public void setLoadFailedView(int loadFailedId) {
-        setLoadFailedView(Util.inflate(mContext, loadFailedId));
-    }
-
-    /**
-     * 初始化全部加载完成布局
-     *
-     * @param loadEndView
-     */
-    public void setLoadEndView(View loadEndView) {
-        mLoadEndView = loadEndView;
-        addFooterView(mLoadEndView);
-    }
-
-    public void setLoadEndView(int loadEndId) {
-        setLoadEndView(Util.inflate(mContext, loadEndId));
-    }
-
-    /**
-     * 初始化emptyView
-     *
-     * @param emptyView
-     */
-    public void setEmptyView(View emptyView) {
-        mEmptyView = emptyView;
-    }
-
-    /**
-     * 返回 footer view数量
-     *
-     * @return
-     */
-    public int getFooterViewCount() {
-        return mOpenLoadMore && !mDatas.isEmpty() ? 1 : 0;
-    }
-
-
-    public void setOnLoadMoreListener(OnLoadMoreListener loadMoreListener) {
-        mLoadMoreListener = loadMoreListener;
     }
 
     public void setOnItemClickListener(OnItemClickListeners<T> itemClickListener) {
--- app/src/main/java/com/othershe/recyclerviewadapter/MainActivity.java
+++ app/src/main/java/com/othershe/recyclerviewadapter/MainActivity.java
@@ -1,107 +1,41 @@
 package com.othershe.recyclerviewadapter;
 
-import android.os.Handler;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Toast;
+import android.widget.Button;
 
-import com.othershe.baseadapter.OnItemClickListeners;
-import com.othershe.baseadapter.OnLoadMoreListener;
-import com.othershe.baseadapter.ViewHolder;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-import java.util.ArrayList;
-import java.util.List;
-
-public class MainActivity extends AppCompatActivity {
-
-    private RefreshAdapter mAdapter;
-
-    private RecyclerView mRecyclerView;
-
-    private boolean isFailed = true;
+    private Button button1;
+    private Button button2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mRecyclerView = (RecyclerView) findViewById(R.id.recyclerview);
 
-        //初始化adapter
-        mAdapter = new RefreshAdapter(this, null, true);
+        button1 = (Button) findViewById(R.id.button1);
+        button2 = (Button) findViewById(R.id.button2);
 
-        //初始化EmptyView
-        View emptyView = LayoutInflater.from(this).inflate(R.layout.empty_layout, (ViewGroup) mRecyclerView.getParent(), false);
-        mAdapter.setEmptyView(emptyView);
-
-        //初始化 开始加载更多的loading View
-        mAdapter.setLoadingView(R.layout.load_loading_layout);
-
-        //设置加载更多触发的事件监听
-        mAdapter.setOnLoadMoreListener(new OnLoadMoreListener() {
-            @Override
-            public void onLoadMore(boolean isReload) {
-                loadMore();
-            }
-        });
-
-        //设置item点击事件监听
-        mAdapter.setOnItemClickListener(new OnItemClickListeners<String>() {
-
-            @Override
-            public void onItemClick(ViewHolder viewHolder, String data, int position) {
-                Toast.makeText(MainActivity.this, data, Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
-        layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
-        mRecyclerView.setLayoutManager(layoutManager);
-
-        mRecyclerView.setAdapter(mAdapter);
-
-
-        //延时3s刷新列表
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                List<String> data = new ArrayList<>();
-                for (int i = 0; i < 12; i++) {
-                    data.add("item--" + i);
-                }
-                //刷新数据
-                mAdapter.setNewData(data);
-            }
-        }, 3000);
+        button1.setOnClickListener(this);
+        button2.setOnClickListener(this);
     }
 
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.button1:
+                start(CommonItemActivity.class);
+                break;
+            case R.id.button2:
+                start(MulitiItemActivity.class);
+                break;
+        }
+    }
 
-    private void loadMore() {
-
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-
-                if (mAdapter.getItemCount() > 15 && isFailed) {
-                    isFailed = false;
-                    //加载失败，更新footer view提示
-                    mAdapter.setLoadFailedView(R.layout.load_failed_layout);
-                } else if (mAdapter.getItemCount() > 17) {
-                    //加载完成，更新footer view提示
-                    mAdapter.setLoadEndView(R.layout.load_end_layout);
-                } else {
-                    final List<String> data = new ArrayList<>();
-                    for (int i = 0; i < 2; i++) {
-                        data.add("item--" + (mAdapter.getItemCount() + i - 1));
-                    }
-                    //刷新数据
-                    mAdapter.setLoadMoreData(data);
-                }
-            }
-        }, 2000);
+    private void start(Class<?> cls) {
+        startActivity(new Intent(this, cls));
     }
 }
